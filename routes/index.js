e0710e9091619d4fcb6a2aa74374770bcd703890
rayman <lvrunjia@gmail.com>
2017-07-09 14:34:10 +0800

    完成1.0.0，3个接口

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var bdresult = [];
 var wbresult = [];
+var blresult = [];
 
 router.get('/list', function(req, res, next) {
   res.append('Access-Control-Allow-Origin', '*');
@@ -13,6 +14,9 @@ router.get('/list', function(req, res, next) {
     return;
   }else if (keyword === 'wbhotsearch'){
     res.json(wbresult);
+    return;
+  }else if (keyword === 'bhotplay'){
+    res.json(blresult);
     return;
   }
   res.json([]);
@@ -27,6 +31,9 @@ router.get('/entrance', function(req, res, next) {
     }, {
       name: '微博热搜',
       keyword: 'wbhotsearch'
+    }, {
+      name: 'B站热播',
+      keyword: 'bhotplay'
     }
   ];
   res.json(result);
@@ -41,16 +48,19 @@ function getwb() {
       'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
     }
   }, function (err, response, body) {
-    if (!err && response.statusCode == 200) {
+    if (!err && response.statusCode === 200) {
       $ = cheerio.load(body, {decodeEntities: false});
       var result = [];
-      $('#pl_searchIndex .list_a>li').each(function (key, val) {
+      $('#pl_searchIndex .list_a > li').each(function (key, val) {
         var obj = {};
         var ele1 = $(val).find('a');
         var ele2 = $(val).find('a>span');
-        obj['title'] = (key+1) + '. ' + ele2.html();
+        var ele3 = $(ele2).find('em').remove();
+        obj['title'] = ele2.text().trim();
         obj['href'] = 'http://s.weibo.com' + ele1.attr('href');
+        obj['hot'] = +ele3.text();
         obj['desc'] = '';
+        obj['pic'] = '';
         result.push(obj);
         if (key > 8) {
           return false;
@@ -58,6 +68,38 @@ function getwb() {
       });
       if (result.length !== 0) {
         wbresult = result;
+      }
+    }
+    setTimeout(function () {
+      getwb();
+    }, 1000 * 60 * 2);
+  });
+}
+getbl();
+function getbl() {
+  request({
+    url: 'https://api.bilibili.com/x/web-interface/ranking?rid=0&day=1',
+    encoding: null,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+    }
+  }, function (err, response, body) {
+    if (!err && response.statusCode === 200) {
+      var result = [];
+      var originJson = JSON.parse(body);
+      var list = originJson.data.list;
+      for (var i = 0; i < 10; i++) {
+        var val = list[i];
+        var obj = {};
+        obj['title'] = val.title;
+        obj['href'] = 'https://m.bilibili.com/video/av' + val.aid + '.html';
+        obj['desc'] = '';
+        obj['hot'] = val.play;
+        obj['pic'] = val.pic;
+        result.push(obj);
+      }
+      if (result.length !== 0) {
+        blresult = result;
       }
     }
     setTimeout(function () {
@@ -75,13 +117,13 @@ function getbd() {
       'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
     }
   }, function (err, response, body) {
-    if (!err && response.statusCode == 200) {
+    if (!err && response.statusCode === 200) {
       var result = [];
       var originJson = JSON.parse(body);
       for (var i = 0; i < 10; i++) {
         var val = originJson.result.topwords[i];
         var obj = {};
-        obj['title'] = (i+1) + '. ' + val.keyword;
+        obj['title'] = val.keyword;
         obj['hot'] = val.searches;
         result.push(obj);
       }
@@ -91,6 +133,7 @@ function getbd() {
         var obj = result[j];
         obj['href'] = val.content.data[0].originlink;
         obj['desc'] = val.content.data[0].description;
+        obj['pic'] = val.content.data[0].image;
       }
       if (result.length !== 0) {
         bdresult = result;
